2a824405ee03daf2142edf295de225ce22372a90
이은석 <modelism93@naver.com>
2023-07-11 00:50:07 +0900

    TypeScript 학습
    - 타입 명시
    - 템플릿 리터럴
    - 블록 스코프(let, const)

--- ssafy-react/00-typescript/src/02-template-literal.ts
+++ ssafy-react/00-typescript/src/02-template-literal.ts
@@ -1,13 +1,17 @@
 // --------------------------------------------------------------------------
 // template literal
 
+
 {
   const koreanFoods: {
+    // 이부분이 타입 설정 부분
     caption: string;
+    // rows : 배열
     rows: {
       headline: string;
       content: string;
     }[];
+
   } = {
     caption: '한식 메뉴',
     rows: [
@@ -19,14 +23,15 @@
 
   function run(): void {
     let rendredResult = printTableHTML(koreanFoods);
-    rendredResult = removeSpaceString(rendredResult);
+    rendredResult = removeSpaceString(rendredResult); // 공백 제거 함수 - 압축된 형태로 보여주고 싶을 때 사용
     console.log(rendredResult);
-    // return undefined;
+    // return undefined; 
+    // 자바스크립트에서는 반환값이 명시적이지 않을 때 undefined이 반환, void 사용해서 타입 설정
   }
 
   function removeSpaceString(data: string): string {
     return data
-      .replace(/(\s+<$|>\s+)/g, ($1) => {
+      .replace(/(\s+<$ | >\s+)/g, ($1) => {
         if (/\s+<$/.test($1)) {
           return '<';
         } else if (/>\s+/.test($1)) {
@@ -46,24 +51,26 @@
     }[];
   }): string {
     return `
+    
       <table class="table">
         <caption class="sr-only">${data.caption}</caption>
-        ${data.rows
-          .map(
-            (item) =>
-              `
+        ${
+          data.rows.map((item) =>
+            `
               <tr>
                 <th>${item.headline}</th>
                 <td>${item.content}</td>
               </tr>
             `
-          )
-          .join('')}
+          ).join('')
+        }
       </table>
+
     `;
   }
 
   function renderTable(data: {
+    // 똑같이 타입 명시
     caption: string;
     rows: {
       headline: string;
