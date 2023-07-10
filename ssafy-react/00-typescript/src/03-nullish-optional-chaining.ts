f31d65c2fc222aba7cf51ae4ff2ca8ac9ae7d718
이은석 <modelism93@naver.com>
2023-07-11 01:28:25 +0900

    TypeScript 학습
    - null 병합 연산자 (??)
    - 옵셔널 체이닝 (?.)

--- ssafy-react/00-typescript/src/03-nullish-optional-chaining.ts
+++ ssafy-react/00-typescript/src/03-nullish-optional-chaining.ts
@@ -7,25 +7,36 @@
     optionalChaining();
   }
 
-  function nullish() {
-    let value = 0; // undefined
 
+
+
+  function nullish() {
+    // let value // undefined // isNullOrUndefined :  100   nullish :  100
+    let value = 0 // isNullOrUndefined :  0   nullish :  0
+    
+    // 이부분은 문제 파트
     let result: number | undefined = value || 100;
     console.log('|| : ', result);
+
 
     function isNullOrUndefined(value: unknown) {
       return value === null || value === undefined ? true : false;
     }
 
+    // 실수 방지를 위한 함수 : isNullOrUndefined
     result = !isNullOrUndefined(value) ? value : 100;
     console.log('isNullOrUndefined : ', result);
 
     // 코드를 작성합니다.
-    // null 병합 연산자 활용
-    result = value ?? 100;
+    // null 병합 연산자 활용 : ??
+    result = value ?? 100; // value가 null아면 100 값을 result에 넣는다
     console.log('nullish : ', result);
   }
 
+
+
+
+  
   // 사용자 정의 타입
   type Topic = {
     _title: string;
@@ -50,6 +61,7 @@
       if (typeof topic.getTitle === 'function') {
         title = topic.getTitle();
       }
+
       // if (typeof topic.getName === 'function') {
       // name = topic.getName();
       // }
@@ -57,7 +69,9 @@
       console.log('typeof : ', name);
 
       // 코드를 작성합니다.
-      name = topic.getName?.();
+      name = topic.getName?.(); // optional chaining으로 getName속성이 함수라면 실행해라 라는 것
+      // if 블럭으로 확인 안해도 오류 확인이 되는 것이다.
+      // 위의 주석 처리와 같은 것
     }
 
     let title, name;
