2a7e33ab0bd8ac67bf33e2e13296db666d2222c8
이은석 <modelism93@naver.com>
2023-07-26 22:55:00 +0900

    lifecycle 과 useEffect 2

--- React(코딩애플)/shop/src/routes/Detail.js
+++ React(코딩애플)/shop/src/routes/Detail.js
@@ -16,28 +16,46 @@ import { useParams } from "react-router-dom";
 
 function Detail(props){
   // useEffect써서 컴포넌트에 갈고리 다는 방법
-  useEffect(()=>{
-    // mount,update시 여기 코드 실행
-    console.log('안녕')
-  })
-
+  
   let [count, setCount] = useState(0)
-
-
-
+  let [alert, setAlert] = useState(true)
   let { id } = useParams();
   let shoe = props.shoes.find((x)=>{ return x.id == id })
   id = Number(id) + 1
   
+  
+  useEffect(()=>{
+    let a = setTimeout(()=>{ setAlert(false) },2000) 
+
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [count]) 
+
+  // useEffect(()=>{}) // 1. 재렌더링마다 코드 실행하고 싶으면
+  // useEffect(()=>{},[]) // 2. mount시 1회 코드 실행하고 싶으면
+  // // 3. unmount시 1회 코드 실행하고 싶으면
+  // useEffect(()=>{
+  //   return ()=>{
+
+  //   }
+  // }, [])
+  // // 4. useEffect 전에 무엇인가를 실행하려면 언제나 return()=>{}
+  // // 5. 특정 state 변경시에만 실행하려면 [state명]
+
   return(
     <div className="container">
+      {
+        alert == true ? <div className="alert alert-warning">2초 이내 구매 시 할인</div>
+        : null
+      }
+      
 
       {count}
       <button onClick={()=>{ setCount(count+1) }}>버튼</button>
       
       <div className="row">
         <div className="col-md-6">
-          
           <img src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg"} width="100%"/>
         </div>
         <div className="col-md-6 mt-4">
